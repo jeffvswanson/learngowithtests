52161652d7623770274ab6af455eaae1287436a4
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-22 11:54:28 -0600

    Add tail sum for slices of integers

--- 04_arrays-and-slices/sum.go
+++ 04_arrays-and-slices/sum.go
@@ -8,11 +8,27 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+// Sums all integers in each slice and returns the sum of each of the slices.
 func SumAll(slicesToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range slicesToSum {
 		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// Sums all elements of a slice except the integer in the 0th index position.
+func SumAllTails(slicesToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range slicesToSum {
+		if len(numbers) > 0 {
+			tail := Sum(numbers[1:])
+			sums = append(sums, tail)
+		} else {
+			sums = append(sums, 0)
+		}
 	}
 	return sums
 }
--- 04_arrays-and-slices/sum_test.go
+++ 04_arrays-and-slices/sum_test.go
@@ -39,3 +39,22 @@ func TestSumAll(t *testing.T) {
 		}
  	})
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+	t.Run("tail sum several slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 9, 0})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("tail sum empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
