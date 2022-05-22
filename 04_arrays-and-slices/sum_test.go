41c90d0f1f14a655dd3de5d3002c714bfff65b09
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-22 11:20:22 -0600

    Adjust Sum to add slices rather than arrays

--- 04_arrays-and-slices/sum_test.go
+++ 04_arrays-and-slices/sum_test.go
@@ -3,11 +3,13 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("slice of integers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d, want %d, given %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d, want %d, given %v", got, want, numbers)
+		}
+	})
 }
