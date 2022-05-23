9e1d80311ebff3ab0014606e60f6211e6d6f5b5b
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-23 17:12:37 -0600

    Add Shape interface

--- 05_structs-methods-and-interfaces/shapes.go
+++ 05_structs-methods-and-interfaces/shapes.go
@@ -2,6 +2,11 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
 type Circle struct {
 	Radius float64
 }
--- 05_structs-methods-and-interfaces/shapes_test.go
+++ 05_structs-methods-and-interfaces/shapes_test.go
@@ -7,39 +7,41 @@ import (
 
 
 func TestPerimeter(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Perimeter()
-		want := 40.0
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Perimeter()
-		want := 2 * math.Pi * circle.Radius
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
 		if got != want {
 			t.Errorf("got %g, want %g", got, want)
 		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		want := 40.0
+		checkPerimeter(t, rectangle, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		want := 2 * math.Pi * circle.Radius
+		checkPerimeter(t, circle, want)
 	})
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := math.Pi * math.Pow(circle.Radius, 2)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g, want %g", got, want)
 		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		want := 100.0
+		checkArea(t, rectangle, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := math.Pi * math.Pow(circle.Radius, 2)
+		checkArea(t, circle, want)
 	})
 }
