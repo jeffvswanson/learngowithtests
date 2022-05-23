c6ba83e1ce77851ef740dbecc431c7eb48ad9b9f
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-23 17:43:14 -0600

    Add table driven tests for shapes

--- 05_structs-methods-and-interfaces/shapes.go
+++ 05_structs-methods-and-interfaces/shapes.go
@@ -31,3 +31,19 @@ func (r Rectangle) Perimeter() float64 {
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Perimeter() float64 {
+	b := t.Base
+	h := t.Height
+	c := math.Sqrt(math.Pow(b, 2) + math.Pow(h, 2))
+	return b + h + c
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
--- 05_structs-methods-and-interfaces/shapes_test.go
+++ 05_structs-methods-and-interfaces/shapes_test.go
@@ -7,41 +7,35 @@ import (
 
 
 func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Perimeter()
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
+	perimeterTests := []struct{
+		shape Shape
+		want float64
+	}{
+		{shape: Circle{Radius: 10.0}, want: 2 * math.Pi * Circle{10.0}.Radius},
+		{shape: Rectangle{Width: 10.0, Height: 10.0}, want: 40.0},
+		{shape: Triangle{Base: 3.0, Height: 4.0}, want: 12.0},
+	}
+	for _, tt := range perimeterTests {
+		got := tt.shape.Perimeter()
+		if got != tt.want {
+			t.Errorf("got %g, want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 40.0
-		checkPerimeter(t, rectangle, want)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		want := 2 * math.Pi * circle.Radius
-		checkPerimeter(t, circle, want)
-	})
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
+	areaTests := []struct{
+		shape Shape
+		want float64
+	}{
+		{shape: Circle{Radius: 10.0}, want: math.Pi * math.Pow(Circle{10.0}.Radius, 2)},
+		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g, want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 100.0
-		checkArea(t, rectangle, want)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := math.Pi * math.Pow(circle.Radius, 2)
-		checkArea(t, circle, want)
-	})
 }
