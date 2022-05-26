19f6d7285f60213a78b72d0c11d4b23df9869201
jeffvswanson <jeffery.swanson@mckesson.com>
2022-05-26 11:28:01 -0600

    Add check for key in map

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("word in dictionary", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+	t.Run("word not in dictionary", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "word not in dictionary"
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+		assertStrings(t, err.Error(), want)
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q, want %q, given %q", got, want, "test")
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
