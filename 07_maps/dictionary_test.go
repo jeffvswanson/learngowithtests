58c90b56867ef589e598ae4c192cf0a2b0b9fdec
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-26 20:02:58 -0600

    Add dictionary.Add method

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	t.Run("word in dictionary", func(t *testing.T) {
@@ -13,6 +14,34 @@ func TestSearch(t *testing.T) {
 		_, err := dictionary.Search("unknown")
 		assertError(t, err, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new definition")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatalf("should find definition for word %q, error: %q", word, err)
+	}
+	assertStrings(t, got, definition)
 }
 
 func assertError(t testing.TB, got, want error) {
