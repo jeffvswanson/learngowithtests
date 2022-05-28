78c55132c8673daeddf63b2720e73087613a8c7b
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-28 14:12:17 -0600

    Complete lesson for concurrency

--- 10_concurrency/checkwebsites_test.go
+++ 10_concurrency/checkwebsites_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -24,7 +25,23 @@ func TestCheckWebsites(t *testing.T) {
 		"waat://furhurterwe.geds":    false,
 	}
 	got := CheckWebsites(mockWebsiteChecker, websites)
-	if !reflect.deepEqual(got, want) {
+	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("want %v, got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
