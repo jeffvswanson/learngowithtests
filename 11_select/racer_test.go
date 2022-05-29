feb410c253909509f2a41deca54091b9fc536dfe
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-29 15:12:40 -0600

    Initialize and set up select directory

---
+++ 11_select/racer_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowUrl := "http://www.facebook.com"
+	fastUrl := "http://www.quii.dev"
+
+	want := fastUrl
+	got := Racer(slowUrl, fastUrl)
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
