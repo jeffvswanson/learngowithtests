c301947226501e5f371d0b8294e14f796f18baef
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-29 15:55:56 -0600

    Add httptest servers

--- 11_select/racer_test.go
+++ 11_select/racer_test.go
@@ -1,8 +1,21 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	slowUrl := "http://www.facebook.com"
 	fastUrl := "http://www.quii.dev"
 
@@ -11,4 +24,6 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
