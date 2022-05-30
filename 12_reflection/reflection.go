464aa568a24a57dab1e06340444e24896ad833b9
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-30 16:20:12 -0600

    Allow multiple strings to be processed from a struct

--- 12_reflection/reflection.go
+++ 12_reflection/reflection.go
@@ -1,3 +1,12 @@
 package main
 
-func walk(x interface{}, fn func(input string)) {}
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
+}
--- 12_reflection/reflection_test.go
+++ 12_reflection/reflection_test.go
@@ -1,19 +1,41 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Jeff"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d, want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Jeff"},
+			[]string{"Jeff"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Jeff", "Denver"},
+			[]string{"Jeff", "Denver"},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
