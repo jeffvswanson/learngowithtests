ff3f86af5e69dadb7c7a3505508c5822185adc59
jeffvswanson <jeff.v.swanson@gmail.com>
2022-05-31 20:34:03 -0600

    Complete sync lesson

--- 13_sync/sync_test.go
+++ 13_sync/sync_test.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 		assertCounter(t, counter, 3)
 	})
+	t.Run("concurrent safe", func(t *testing.T) {
+		want := 1000
+		var wg sync.WaitGroup
+		wg.Add(want)
+
+		counter := NewCounter()
+		for i := 0; i < want; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, want)
+	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
