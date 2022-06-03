d2e36f36c07063282e9166c060b15c4aa6503f5f
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-03 17:11:57 -0600

    Add test cases to numeral_test.go

--- 15_property-based-testing/numeral_test.go
+++ 15_property-based-testing/numeral_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(i)
-	want := "I"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 -> I", 1, "I"},
+		{"2 -> II", 2, "II"},
+	}
+	for _, tc := range cases {
+		t.Run(tc.Description, func(t *testing.t) {
+			got := ConvertToRoman(tc.Arabic)
+			if got != tc.Want {
+				t.Errorf("got %q, want %q", got, tc.Want)
+			}
+		})
 	}
 }
