26b23167e0e1dd616f7e68dcfd40865412c5afbe
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-03 17:25:47 -0600

    Convert numbers up to 8

--- 15_property-based-testing/numeral_test.go
+++ 15_property-based-testing/numeral_test.go
@@ -11,6 +11,11 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 -> I", 1, "I"},
 		{"2 -> II", 2, "II"},
 		{"3 -> III", 3, "III"},
+		{"4 -> IV", 4, "IV"},
+		{"5 -> V", 5, "V"},
+		{"6 -> VI", 6, "VI"},
+		{"7 -> VII", 7, "VII"},
+		{"8 -> VIII", 8, "VIII"},
 	}
 	for _, tc := range cases {
 		t.Run(tc.Description, func(t *testing.T) {
