be2efbfb4292e0bfb7f3f384d3ace69626b093ac
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-06 21:38:11 -0600

    WIP Add hand representation in radians

--- 16_maths/clockface_test.go
+++ 16_maths/clockface_test.go
@@ -1,17 +1,59 @@
-package clockface_test
+package clockface
 
 import (
-	"github.com/gypsydave5/learn-go-with-tests/math/v1/clockface"
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+func TestSecondHand(t *testing.T) {
+	cases := []struct {
+		condition string
+		Time      time.Time
+		Position  Point
+	}{
+		{
+			"hand at midnight",
+			time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC),
+			Point{X: 150, Y: 150 - 90},
+		},
+		{
+			"hand at 30 seconds",
+			time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC),
+			Point{X: 150, Y: 150 + 90},
+		},
 	}
+	for _, tc := range cases {
+		t.Run(tc.condition, func(t *testing.T) {
+			got := SecondHand(tc.Time)
+			if got != tc.Position {
+				t.Errorf("got %v, want %v", got, tc.Position)
+			}
+		})
+	}
+}
+
+func TestHandPositionInRadians(t *testing.T) {
+	cases := []struct {
+		condition string
+		time      time.Time
+		angle     float64
+	}{
+		{"0 seconds", simpleTime(0, 0, 0), 0},
+		{"30 seconds", simpleTime(0, 0, 30), math.Pi},
+		{"45 seconds", simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{"7 seconds", simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+	for _, tc := range cases {
+		t.Run(tc.condition, func(t *testing.T) {
+			got := handPositionInRadians(tc.time)
+			if got != tc.angle {
+				t.Fatalf("got %v radians, want %v", got, tc.angle)
+			}
+		})
+	}
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(1337, time.January, 1, hours, minutes, seconds, 0, time.UTC)
 }
--- 16_maths/go.mod
+++ 16_maths/go.mod
@@ -1,5 +1,3 @@
 module github.com/jeffvswanson/learngowithtests/16_maths/clockface
 
 go 1.18
-
-require github.com/quii/learn-go-with-tests v0.0.0-20220605080514-f28964aa0996 // indirect
