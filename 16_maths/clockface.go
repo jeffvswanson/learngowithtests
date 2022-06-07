74f5239f6a92a3e831cb9c76a52e7362017fe5c5
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-06 21:38:11 -0600

    WIP Add hand representation in radians

--- 16_maths/clockface.go
+++ 16_maths/clockface.go
@@ -1,1 +1,25 @@
 package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two-dimensional Cartesian coordinate.
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analog clock at time 't'
+// represented by a Point.
+func SecondHand(t time.Time) Point {
+	p := handPositionInRadians(t)
+	return Point{150, p}
+}
+
+// Provides a clock hand position in radians
+func handPositionInRadians(t time.Time) float64 {
+	// One second is represented on an analog clock face with 2*pi/60 radians/second.
+	return float64(t.Second()) * (math.Pi / 30)
+}
