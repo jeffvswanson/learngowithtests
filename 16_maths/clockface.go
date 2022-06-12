06335dc5a0baf0e41b7ace576766f169d2395d20
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-12 17:18:32 -0600

    Add constants for partitions around a clock

--- 16_maths/clockface.go
+++ 16_maths/clockface.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
+const (
+	secondsInHalfClock = 30
+	secondsInClock     = 2 * secondsInHalfClock
+	minutesInHalfClock = 30
+	minutesInClock     = 2 * minutesInHalfClock
+	hoursInHalfClock   = 6
+	hoursInClock       = 2 * hoursInHalfClock
+)
+
 // A Point represents a two-dimensional Cartesian coordinate.
 type Point struct {
 	X float64
@@ -14,7 +23,7 @@ type Point struct {
 // secondsInRadians provides the second hand of a clock's hand position in radians relative to 12 o'clock.
 func secondsInRadians(t time.Time) float64 {
 	// One second is represented on an analog clock face with 2*pi/60 radians/second.
-	return math.Pi / (30 / float64(t.Second()))
+	return math.Pi / (secondsInHalfClock / float64(t.Second()))
 }
 
 // deriveSecondHandPoint determines the end point of a clock's second hand given the time on a unit circle.
@@ -25,7 +34,7 @@ func deriveSecondHandPoint(t time.Time) Point {
 
 // minutesInRadians provides the minute hand position on a clock in radians relative to 12 o'clock.
 func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
+	return (secondsInRadians(t) / secondsInClock) + (math.Pi / (minutesInHalfClock / float64(t.Minute())))
 }
 
 // deriveMinuteHandPoint provides the points a clock's minute hand should be at given a time.
@@ -36,7 +45,7 @@ func deriveMinuteHandPoint(t time.Time) Point {
 
 // hoursInRadians provides the hour hand angle on a clock in radians relative to 12 o'clock.
 func hoursInRadians(t time.Time) float64 {
-	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / hoursInClock) + (math.Pi / (hoursInHalfClock / float64(t.Hour()%hoursInClock)))
 }
 
 // deriveHourHandPoint provides the points a clock's hour hand should be at given a time.
