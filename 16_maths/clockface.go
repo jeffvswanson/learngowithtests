e8bda818ab2be5e2b6915352a21832122023e21c
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-12 11:17:17 -0600

    Refactor to DRY up angle -> point calculations

--- 16_maths/clockface.go
+++ 16_maths/clockface.go
@@ -20,12 +20,23 @@ func secondsInRadians(t time.Time) float64 {
 // Determines the end point of a clock's second hand given the time on a unit circle.
 func deriveSecondHandPoint(t time.Time) Point {
 	angle := secondsInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-	return Point{x, y}
+	return derivePointFromAngle(angle)
 }
 
 // Provides the minute hand position on a clock in radians
 func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
+}
+
+// Provides the points a clock's minute hand should be at given a time.
+func deriveMinuteHandPoint(t time.Time) Point {
+	angle := minutesInRadians(t)
+	return derivePointFromAngle(angle)
+}
+
+// derivePointFromAngle build the end point of a hand of a clock represented with a unit circle given the angle of the hand from the 12 o'clock position
+func derivePointFromAngle(angle float64) Point {
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
 }
