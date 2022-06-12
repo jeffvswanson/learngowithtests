4d64dcfab3252a98b1d1963b185262e870cedea4
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-12 16:30:26 -0600

    Refactor clockhand making code

--- 16_maths/svgWriter.go
+++ 16_maths/svgWriter.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	minuteHandLength = 80
 	secondHandLength = 90
 	clockCenterX     = 150
 	clockCenterY     = 150
@@ -16,19 +17,34 @@ const (
 func SVGWriter(w io.Writer, t time.Time) {
 	io.WriteString(w, svgStart)
 	io.WriteString(w, bezel)
-	SecondHand(w, t)
+	secondHand(w, t)
+	minuteHand(w, t)
 	io.WriteString(w, svgEnd)
 }
 
 // SecondHand is the unit vector of the second hand of an analog clock at time 't'
 // represented by a Point. Scales an analog clock's second hand to represent the time on the clock and the
 // point the second hand should be at on the clockface as represented on an SVG image.
-func SecondHand(w io.Writer, t time.Time) {
-	p := deriveSecondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
-	p = Point{p.X, -p.Y}                                      // flip
-	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
+func secondHand(w io.Writer, t time.Time) {
+	p := makeHand(deriveSecondHandPoint(t), secondHandLength)
 	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+// minuteHand is the unit vector of the minute hand on an analog clock at time 't'
+// represented by a Point. Scales an analog clock's minute hand to represent the time on the clock and the
+// point the minute hand should be at on the clockface as represented on an SVG image.
+func minuteHand(w io.Writer, t time.Time) {
+	p := makeHand(deriveMinuteHandPoint(t), minuteHandLength)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+// makeHand performs the scale, flip, and translate needed to convert a unit circle based point into a point
+// represented on the clockface.
+func makeHand(p Point, length float64) Point {
+	p = Point{p.X * length, p.Y * length}             // scale
+	p = Point{p.X, -p.Y}                              // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY} // translate
+	return p
 }
 
 const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
