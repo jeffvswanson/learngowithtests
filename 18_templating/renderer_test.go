14312c86338e952fcf6ae88c5416da3bae30c39a
jeffvswanson <jeff.v.swanson@gmail.com>
2022-06-16 15:06:41 -0600

    Add test for the blogrenderer

---
+++ 18_templating/renderer_test.go
@@ -0,0 +1,31 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"github.com/jeffvswanson/learngowithtests/18_templating/blogrenderer"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost := blogrenderer.Post{
+			Title: "Hello World",
+			Body: "This is a post",
+			Description: "very descriptive",
+			Tags: []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("convert a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := buf.String()
+		want := `<h1>Hello World</h1>`
+		if got != want {
+			t.Errorf("got '%s', want '%s", got, want)
+		}
+	})
+}
